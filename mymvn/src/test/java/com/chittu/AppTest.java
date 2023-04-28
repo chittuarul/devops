bb0b46f03147ab399e9d77540ff9a29d06c3419c
localuser <test@email.com>
2023-04-28 04:07:10 +0000

    maven

---
+++ mymvn/src/test/java/com/chittu/AppTest.java
@@ -0,0 +1,21 @@
+package com.chittu;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import java.lang.*;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+   System.out.println("test chittu");
+   assertTrue( true );
+    }
+}
